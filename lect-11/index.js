ea65678c75660a0d67c259e9b7430c6c4305dce3
harshita jain847 <hj.harshitabadkul@gmail.com>
2026-05-24 15:11:00 +0530

    web dev

---
+++ lect-11/index.js
@@ -0,0 +1,15 @@
+// let bn=document.querySelector('body');
+let btn=document.querySelector('button');
+let bn=document.querySelector('body');
+bn.addEventListener("click",function(){
+bn.style.backgroundColor="red"
+let isdark=true;
+if(isdark){
+    bn.style.backgroundColor="red";
+
+}
+else {
+     bn.style.backgroundColor="black";
+}
+}
+)
